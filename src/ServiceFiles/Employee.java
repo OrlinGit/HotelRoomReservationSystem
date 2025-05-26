43b7ab178dd0eb84259e3dfcd2deec1f108b5f62
OrlinGit <orlin.nikolov@hotmail.com>
2025-05-26 17:09:34 +0300

    I greated this files to make the program little bit more abstract.

--- src/ServiceFiles/Employee.java
+++ src/ServiceFiles/Employee.java
@@ -1,4 +1,42 @@
 package ServiceFiles;
 
-public class Employee {
+import java.io.FileNotFoundException;
+
+public class Employee extends User{
+	public Employee() {
+	}
+
+	public Employee(String username, String password) {
+		super(username, password);
+	}
+
+	@Override
+	protected boolean setNewUser() {
+		return super.setNewUser();
+	}
+
+	@Override
+	protected String getUserID(String username, String password) throws FileNotFoundException {
+		return super.getUserID(username, password);
+	}
+
+	protected boolean createNewRoom(){
+
+		return false;
+	}
+
+	protected boolean deleteRoom(String UUID){
+		return false;
+	}
+
+	/*
+	Here the employee will be able to create reservation and manipulate the files by directly manipulating the files.
+	 */
+	protected boolean makeReservation(){
+		return false;
+	}
+
+	protected boolean deleteReservation(){
+		return false;
+	}
 }
